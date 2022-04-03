c36cd0d5e4dcf70f542b3af239d37d1a88cd9ce8
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2022-04-03 17:13:41 +0200

    feat(ci): prepare gh pages

--- deploy-page.sh
+++ deploy-page.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env sh
 # abort on errors
 set -e
-# build
-npm run build:page
-# navigate into the build output directory
+# # build
+NODE_ENV=production npm run build:page
+# # navigate into the build output directory
 cd dist
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+# # if you are deploying to a custom domain
+# # echo 'www.example.com' > CNAME
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:username/hello-world.git main:gh-pages
+
+git push -f git@github.com:c-commerce/charles-json-logic-editor.git main:gh-pages
 cd -
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/charles-json-logic-editor/'
+    : '/',
   chainWebpack: (config) => {
     config.plugin('monaco-editor').use(MonacoWebpackPlugin, [])
   }
